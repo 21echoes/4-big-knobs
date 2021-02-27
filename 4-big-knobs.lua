265eff04a030321a64cf301d7c48f88d6af9df16
David Kettler <21echoes@gmail.com>
2021-02-27 13:54:34 -0800

    Get it working w/o arc, add optional instructional labels

--- 4-big-knobs.lua
+++ 4-big-knobs.lua
@@ -13,6 +13,7 @@ local UI = require 'ui'
 local ControlSpec = require "controlspec"
 local Arcify = include("lib/arcify")
 local UIState = include('lib/ui_state')
+local Label = include("lib/label")
 
 local arc_device = arc.connect()
 local arcify = Arcify.new(arc_device, false)
@@ -20,30 +21,52 @@ local NUM_CONTROLS = 4
 local MIN_VOLTS = -5
 local MAX_VOLTS = 5
 local dials = {}
+local corner_labels = {}
+local dial_focus = 1
 local ui_refresh_metro
-
--- TODO v2: also control via E2&E3
--- TODO beyond: min and max per output are their own params
--- TODO beyond: snapshot values, interpolate
--- TODO beyond: crow inputs do something?
 
 local function param_name_for_ctrl(ctrl)
   return ctrl.."_volt"
 end
 
-local function ctrl_changed(ctrl, value)
-  crow.output[ctrl].volts = value
+local function compute_voltage(ctrl)
+  local v = params:get(param_name_for_ctrl(ctrl))
+  -- return util.clamp(v, params:get("min_volts"), params:get("max_volts"))
+  return util.clamp(v, MIN_VOLTS, MAX_VOLTS)
+end
+
+local function ctrl_changed(ctrl)
+  crow.output[ctrl].volts = compute_voltage(ctrl)
   UIState.set_dirty()
 end
 
+local function minmax_changed()
+  for ctrl=1,NUM_CONTROLS do
+    ctrl_changed(ctrl)
+  end
+end
+
 local function init_params()
+  params:add_group("Crow Outputs", 4)
+  -- params:add_control("min_volts", "Min Volts", ControlSpec.new(MIN_VOLTS, MAX_VOLTS, "lin", 0.01, MIN_VOLTS))
+  -- params:set_action("min_volts", function() minmax_changed() end)
+  -- params:add_control("max_volts", "Max Volts", ControlSpec.new(MIN_VOLTS, MAX_VOLTS, "lin", 0.01, MAX_VOLTS))
+  -- params:set_action("min_volts", function() minmax_changed() end)
   for ctrl=1,NUM_CONTROLS do
     params:add_control(param_name_for_ctrl(ctrl), "Output "..ctrl..": Voltage", ControlSpec.new(MIN_VOLTS, MAX_VOLTS, "lin", 0.01, 0))
-    params:set_action(param_name_for_ctrl(ctrl), function(value)
-      ctrl_changed(ctrl, value)
-    end)
+    params:set_action(param_name_for_ctrl(ctrl), function() ctrl_changed(ctrl) end)
     arcify:register(param_name_for_ctrl(ctrl))
   end
+  params:add_option("show_instructions", "Show instructions?", {"No", "Yes"}, 2)
+  params:add_option("is_shield", "Norns Shield?", {"No", "Yes"}, 1)
+  params:set_action("is_shield", function(value)
+    local top_left = value == 1 and 1 or 2
+    local top_right = value == 1 and 2 or 1
+    corner_labels[top_left].x = 0
+    corner_labels[top_left].align = Label.ALIGN_LEFT
+    corner_labels[top_right].x = 128
+    corner_labels[top_right].align = Label.ALIGN_RIGHT
+  end)
   arcify:add_params()
   for ctrl=1,NUM_CONTROLS do
     arcify:map_encoder_via_params(ctrl, param_name_for_ctrl(ctrl))
@@ -55,17 +78,41 @@ local function redraw()
   screen.move(dials[1].x+5, dials[1].y+20)
   screen.close()
   screen.clear()
-  for ctrl=1,NUM_CONTROLS do
+  for ctrl=1,#dials do
+    -- dials[ctrl].min_value = params:get("min_volts")
+    -- dials[ctrl].max_value = params:get("max_volts")
     dials[ctrl]:set_value(params:get(param_name_for_ctrl(ctrl)))
+    dials[ctrl].active = (dial_focus == 1 and ctrl < 3) or (dial_focus == 2 and ctrl >= 3)
     dials[ctrl]:redraw()
+  end
+  if params:get("show_instructions") == 2 then
+    -- Top left (top right if is_shield)
+    corner_labels[1]:redraw()
+    -- Top right (top left if is_shield)
+    if arc_device then
+      corner_labels[2]:redraw()
+    end
+    -- Bottom left
+    if dial_focus == 1 then
+      corner_labels[3].text = "Dial 1  Dial 2"
+    else
+      corner_labels[3].text = "Dial 3  Dial 4"
+    end
+    corner_labels[3]:redraw()
+    -- Bottom right
+    corner_labels[4]:redraw()
   end
 end
 
 local function init_ui()
-  dials[1] = UI.Dial.new(9, 13, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
-  dials[2] = UI.Dial.new(39, 26, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
-  dials[3] = UI.Dial.new(69, 13, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
-  dials[4] = UI.Dial.new(98.5, 26, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
+  dials[1] = UI.Dial.new(2, 19.5, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
+  dials[2] = UI.Dial.new(36, 19.5, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
+  dials[3] = UI.Dial.new(70, 19.5, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
+  dials[4] = UI.Dial.new(104, 19.5, 22, 0, MIN_VOLTS, MAX_VOLTS, 0.01, 0, {}, 'V')
+  corner_labels[1] = Label.new({x = 0, y = 8, text="E1: Switch Focus"})
+  corner_labels[2] = Label.new({x = 128, y = 8, text="Arc", align=Label.ALIGN_RIGHT})
+  corner_labels[3] = Label.new({x = 0, y = 64, text="Dial 1  Dial 2"})
+  corner_labels[4] = Label.new({x = 128, y = 64, align=Label.ALIGN_RIGHT})
 
   UIState.init_arc {
     device = arc_device,
@@ -89,6 +136,24 @@ local function init_ui()
   ui_refresh_metro:start()
 end
 
+-- function key(n, z)
+--   -- All key presses are routed to the current page's class.
+--   local screen_dirty = false
+--   if current_page() then screen_dirty = current_page():key(n, z) end
+--   ScreenState.mark_screen_dirty(screen_dirty)
+-- end
+
+function enc(n, delta)
+  if n == 1 then
+    dial_focus = util.clamp(dial_focus + delta, 1, 2)
+    UIState.set_dirty()
+  else
+    local dial_index = (dial_focus == 2 and 2 or 0) + n - 1
+    params:delta(param_name_for_ctrl(dial_index), delta)
+    UIState.set_dirty()
+  end
+end
+
 function init()
   init_params()
   init_ui()
@@ -99,4 +164,8 @@ function cleanup()
   params:write()
   metro.free(ui_refresh_metro.id)
   ui_refresh_metro = nil
+end
+
+function rerun()
+  norns.script.load(norns.state.script)
 end
---
+++ lib/label.lua
@@ -0,0 +1,44 @@
+local Label = {
+  ALIGN_LEFT='ALIGN_LEFT',
+  ALIGN_RIGHT='ALIGN_RIGHT',
+  ALIGN_CENTER='ALIGN_CENTER',
+}
+Label.__index = Label
+
+function Label.new(i)
+  i = i or {}
+  setmetatable(i, Label)
+  i.__index = Label
+
+  if i.x == nil then i.x = 0 end
+  if i.y == nil then i.y = 0 end
+  if i.align == nil then i.align = Label.ALIGN_LEFT end
+  if i.level == nil then i.level = 15 end
+  if i.text == nil then i.text = "" end
+  if i.font_face == nil then i.font_face = 1 end
+  if i.font_size == nil then i.font_size = 8 end
+
+  return i
+end
+
+function Label:redraw()
+  screen.move(self.x, self.y)
+  screen.font_face(self.font_face)
+  screen.font_size(self.font_size)
+  screen.level(self.level)
+  if self.align == Label.ALIGN_RIGHT then
+    screen.text_right(self.text)
+  elseif self.align == Label.ALIGN_CENTER then
+    screen.text_center(self.text)
+  else
+    screen.text(self.text)
+  end
+  -- Reset back to defaults
+  screen.font_face(1)
+  screen.font_size(8)
+  screen.level(15)
+  -- Prevent a stray line being drawn
+  screen.stroke()
+end
+
+return Label
